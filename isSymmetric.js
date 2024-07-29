e98be8ca4148033c28cc5c6b4ff4f067a12d62d5
Miroslav Tsarev <mtzarev@gmail.com>
2024-07-29 22:43:04 +0300

    Update isSymmetric.js

--- isSymmetric.js
+++ isSymmetric.js
@@ -1,6 +1,6 @@
 function isSymmetric(arr) {
     if (!Array.isArray(arr)){
-        return false; // Non-arrays are non-symmetric
+        return false;
     }
     let reversed = arr.slice(0).reverse(); // Clone and reverse
     let equal = (JSON.stringify(arr) == JSON.stringify(reversed));
