6f125f202a10b9a85d7c015fa2c5c090ad55dc23
Miroslav Tsarev <mtzarev@gmail.com>
2024-07-29 22:45:13 +0300

    Update isSymmetric.test.js

--- isSymmetric.test.js
+++ isSymmetric.test.js
@@ -36,10 +36,9 @@ describe(`isSymmetric`, ()=>{
         expect(isSymmetric([])).to.be.true
     });
     it (`return true for [5,'hi',{a:5},new Date(),{a:5},'hi',5]`,()=>{
-        expect(isSymmetric([5,'hi',{a:5},new Date(),{a:5},'hi',5])).to.be.true
-    });
+        expect(isSymmetric([5,'hi',{a:5},new Date(),{a:5},'hi',5])).to.be.true});
 });
 
-//describe("isSymmetric(arr)", function () {
+
    
 
